b945ed5471348f312efa563ec6baf50a114d69f4
Shashwat <shashlko2002@gmai.com>
2023-09-02 18:19:07 +0530

    changed asset paths

--- src/views/components/typing_test/layout.jsx
+++ src/views/components/typing_test/layout.jsx
@@ -37,7 +37,7 @@ const Test = (props) => {
     el.classList.remove(name);
   };
 
-  const sound = new Audio("../../../../public/click4_1.wav");
+  const sound = new Audio("/click4_1.wav");
   
 
   const setTimer = () => {
